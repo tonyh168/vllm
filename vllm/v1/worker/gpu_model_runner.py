e86ddbd0c53c0028bd3881be4fc84e6424523941
Your Name <you@example.com>
2026-07-24 18:39:14 +0800

    disable sort and mm

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -222,6 +222,17 @@ AttnMetadataDict: TypeAlias = dict[str, AttentionMetadata]
 # list when ubatching is enabled
 PerLayerAttnMetadata: TypeAlias = list[AttnMetadataDict] | AttnMetadataDict
 
+import os
+if os.getenv("USE_FLAGGEMS", "false").lower() in ("1", "true", "yes"):
+    try:
+        import flag_gems
+        flag_gems.enable(record=True, path="/workspace/flaggems_enable_oplist.txt", unused=["sort", "mm"])
+        logger.info("Successfully enabled flag_gems as default ops implementation.")
+    except ImportError:
+        logger.warning("Failed to import 'flag_gems'. Falling back to default implementation.")
+    except Exception as e:
+        logger.warning(f"Failed to enable 'flag_gems': {e}. Falling back to default implementation.")
+
 
 # Wrapper for ModelRunnerOutput to support overlapped execution.
 class AsyncGPUModelRunnerOutput(AsyncModelRunnerOutput):
